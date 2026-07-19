848a521b686f9e58cc8478b77d8cf1602cb475a7
Victor Aranha <aldo.victor1610@gmail.com>
2026-07-19 17:53:48 -0300

    🐛 fix webhook_url

--- src/_lib/constants.ts
+++ src/_lib/constants.ts
@@ -7,5 +7,6 @@ export const PRESIDENTE = {
 };
 
 export const HANDLE = 'aranhavictor';
-export const WEBHOOK_URL = 'https://.../api/transacoes';
+export const WEBHOOK_URL =
+  'https://um-numero-um-voto-theta.vercel.app/api/transacoes';
 export const PRICE = 100; // (R$ 1,00 em centavos)
